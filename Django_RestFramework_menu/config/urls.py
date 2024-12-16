5f4a633317990569d859028d77024b737ef1f06a
Davron <152484311+Davron030901@users.noreply.github.com>
2024-12-16 15:57:59 +0500

    Update urls.py
    new url

--- Django_RestFramework_menu/config/urls.py
+++ Django_RestFramework_menu/config/urls.py
@@ -20,4 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('menu.urls')),
+    path('api/',include('api.urls')),
+    path('base/',include('base.urls')),
 ]
